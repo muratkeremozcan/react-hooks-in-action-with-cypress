70482a5eb5858f6abce036beaf04fbbabcd2e818
Murat Ozcan <muratkerem@gmail.com>
2022-01-31 09:12:53 -0600

    ch3.2 done

---
+++ cypress/integration/routes.spec.js
@@ -0,0 +1,17 @@
+before(() => cy.visit('/'))
+it('Bookings', () => {
+  cy.contains('Bookings').click()
+  cy.url().should('contain', '/bookings')
+  cy.get('.bookings-page').should('be.visible')
+})
+it('Bookables', () => {
+  cy.contains('Bookables').click()
+  cy.url().should('contain', '/bookables')
+  cy.get('.bookables-page')
+})
+
+it('Users', () => {
+  cy.contains('Users').click()
+  cy.url().should('contain', '/users')
+  cy.get('.users-page')
+})
