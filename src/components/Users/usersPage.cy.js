1aaa2853886b02c0b4b65dfff7cbb9a6a090fbb1
Murat Ozcan <muratkerem@gmail.com>
2022-02-06 06:22:39 -0600

    stubbed usersPage network call

--- src/components/Users/usersPage.cy.js
+++ src/components/Users/usersPage.cy.js
@@ -3,6 +3,9 @@ import UsersPage from './UsersPage'
 import '../../App.css'
 
 it('renders UsersPage', () => {
+  cy.intercept('GET', 'http://localhost:3001/users', {
+    fixture: 'users'
+  }).as('userStub')
   mount(<UsersPage />)
   cy.getByCy('users-list').should('be.visible')
 })
