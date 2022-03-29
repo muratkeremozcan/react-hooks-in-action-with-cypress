70a6401720e6673b6c7fa84c886609395a5eb888
Murat Ozcan <muratkerem@gmail.com>
2022-03-29 11:19:55 -0500

    added readme for feature flags

--- cypress/support/ff-helper.js
+++ cypress/support/ff-helper.js
@@ -17,3 +17,11 @@ export const removeTarget = (featureFlagKey, targetIndex = 0) =>
     featureFlagKey,
     targetIndex
   })
+
+const { _ } = Cypress
+
+/** Generates n alphanumeric characters */
+const nChars = (n) => _.times(n, () => _.random(35).toString(36)).join('')
+
+export const randomId = () =>
+  `${nChars(8)}-${nChars(4)}-${nChars(4)}-${nChars(4)}-${nChars(12)}`
