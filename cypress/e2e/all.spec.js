f57ab86f3d63082a1a5eb9f1998ec0c1e73cd36e
Murat Ozcan <muratkerem@gmail.com>
2022-06-23 16:44:21 +0300

    fixed lock file

---
+++ cypress/e2e/all.spec.js
@@ -0,0 +1,15 @@
+import './feature-flags/bookables-prev-next.spec'
+import './feature-flags/bookings-date-and-week.spec'
+import './feature-flags/bookings-slide-show.spec'
+import './feature-flags/ff-sanity.spec'
+import './feature-flags/set-local-storage-alternative.spec'
+import './feature-flags/users-next-prev.spec'
+
+import './ui-e2e/crud-bookable.spec'
+
+import './ui-integration/bookable-details-retainment.spec'
+import './ui-integration/bookable-list.spec'
+import './ui-integration/direct-nav.spec'
+import './ui-integration/routes.spec'
+import './ui-integration/user-context-retainment.spec'
+import './ui-integration/user-context-retainment-applitools.spec'
