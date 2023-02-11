54c00a07b6576e68ccedd51e548062780a784675
Murat Ozcan <muratkerem@gmail.com>
2023-02-11 06:17:45 -0600

    fail fast false

--- cypress/e2e/ui-integration/user-context-retainment-applitools.spec.js
+++ cypress/e2e/ui-integration/user-context-retainment-applitools.spec.js
@@ -1,48 +1,44 @@
 // Applitools version of the visual test
-describe(
-  'User selection retainment between routes',
-  { tags: '@ui-integration' },
-  () => {
-    beforeEach(() => {
-      // Each test should open its own Eyes for its own snapshots
-      cy.eyesOpen({
-        appName: 'hooks-in-action',
-        testName: Cypress.currentTest.title
-      })
-
-      cy.stubNetwork()
-      cy.visit('/')
+describe('User selection retainment between routes', () => {
+  beforeEach(() => {
+    // Each test should open its own Eyes for its own snapshots
+    cy.eyesOpen({
+      appName: 'hooks-in-action',
+      testName: Cypress.currentTest.title
     })
 
-    it('Should keep the user context between routes', () => {
-      cy.fixture('users').then((users) => {
-        cy.get('.user-picker').select(users[3].name)
-        cy.contains('Users').click()
+    cy.stubNetwork()
+    cy.visit('/')
+  })
 
-        cy.wait('@userStub')
-        cy.url().should('contain', '/users')
-        cy.get('.item-header').contains(users[3].name)
+  it('Should keep the user context between routes', () => {
+    cy.fixture('users').then((users) => {
+      cy.get('.user-picker').select(users[3].name)
+      cy.contains('Users').click()
 
-        // full page test
-        cy.eyesCheckWindow({
-          tag: 'User selection retainment between routes',
-          target: 'window',
-          // if fully is true (default) then the snapshot is of the entire page,
-          // if fully is false then snapshot is of the viewport.
-          fully: false,
-          matchLevel: 'Layout'
-        })
-        // partial page test
-        cy.eyesCheckWindow({
-          tag: 'user details with custom selector',
-          target: 'region',
-          selector: '[data-cy="user-details"]'
-        })
+      cy.wait('@userStub')
+      cy.url().should('contain', '/users')
+      cy.get('.item-header').contains(users[3].name)
+
+      // full page test
+      cy.eyesCheckWindow({
+        tag: 'User selection retainment between routes',
+        target: 'window',
+        // if fully is true (default) then the snapshot is of the entire page,
+        // if fully is false then snapshot is of the viewport.
+        fully: false,
+        matchLevel: 'Layout'
+      })
+      // partial page test
+      cy.eyesCheckWindow({
+        tag: 'user details with custom selector',
+        target: 'region',
+        selector: '[data-cy="user-details"]'
       })
     })
+  })
 
-    afterEach(() => {
-      cy.eyesClose()
-    })
-  }
-)
+  afterEach(() => {
+    cy.eyesClose()
+  })
+})
--- cypress/e2e/ui-integration/user-context-retainment.spec.js
+++ cypress/e2e/ui-integration/user-context-retainment.spec.js
@@ -1,31 +1,44 @@
 // Percy version of the visual test
+describe('User selection retainment between routes', () => {
+  beforeEach(() => {
+    cy.stubNetwork()
+    cy.visit('/')
+  })
 
-describe(
-  'User selection retainment between routes',
-  { tags: '@ui-integration' },
-  () => {
-    beforeEach(() => {
-      cy.stubNetwork()
-      cy.visit('/')
+  it('Should keep the user context between routes', () => {
+    cy.fixture('users').then((users) => {
+      cy.get('.user-picker').select(users[3].name)
+      cy.contains('Users').click()
+
+      cy.wait('@userStub')
+      cy.url().should('contain', '/users')
+      cy.get('.item-header').contains(users[3].name)
+
+      // the visual test
+      cy.percySnapshot('User selection retainment between routes')
+
+      // using custom command for css selector focus
+      cy.getByCy('user-details').percySnapshotElement(
+        'user details with custom selector'
+      )
     })
+  })
+})
 
-    it('Should keep the user context between routes', () => {
-      cy.fixture('users').then((users) => {
-        cy.get('.user-picker').select(users[3].name)
-        cy.contains('Users').click()
+function scope(documentClone, selector) {
+  const element = documentClone.querySelector(selector)
+  documentClone.querySelector('body').innerHTML = element.outerHTML
 
-        cy.wait('@userStub')
-        cy.url().should('contain', '/users')
-        cy.get('.item-header').contains(users[3].name)
-
-        // the visual test
-        cy.percySnapshot('User selection retainment between routes')
-
-        // using custom command for css selector focus
-        cy.getByCy('user-details').percySnapshotElement(
-          'user details with custom selector'
-        )
-      })
+  return documentClone
+}
+Cypress.Commands.add(
+  'percySnapshotElement',
+  { prevSubject: true },
+  (subject, name, options) => {
+    cy.percySnapshot(name, {
+      domTransformation: (documentClone) =>
+        scope(documentClone, subject.selector),
+      ...options
     })
   }
 )
